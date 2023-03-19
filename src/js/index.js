680acd1bff1ea9e9530be73b4b3bde73e72330dc
RafaelContact <contactrodrigues21@gmail.com>
2023-03-19 18:21:42 -0300

    feat: init commit html

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
+
+// Quando um campo estiver preenchido a borda do input deve ficar verde
+
