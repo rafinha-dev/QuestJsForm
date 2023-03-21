07ca2cdcf8ff8e64964f975f77270e2123ac1054
RafaelContact <contactrodrigues21@gmail.com>
2023-03-21 04:24:50 -0300

    fix: fix javaScript the test of the true and false

--- src/js/index.js
+++ src/js/index.js
@@ -28,14 +28,15 @@ form.addEventListener('submit', (event) => {
             "^[0-9]"
     )
 
-    if(nameNumber.length && contentNumber.length < 2){
-        
-        if(emailRegex.test(email) && telRegex.test(tel)){
-            validationFalse() 
-        }   
-    }else{
-        validationTrue()
-    }
+
+    if(nameNumber.length  < 2 || contentNumber.length < 2 ){  
+        validationFalse()
+             
+        }else if(emailRegex.test(email) && telRegex.test(tel)){
+            validationTrue()
+        }else{
+            validationFalse()
+        }
 })
 
 // Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
