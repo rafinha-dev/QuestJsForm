e01261e6dbf7a9586a4303a480f858d49a12696e
RafaelContact <contactrodrigues21@gmail.com>
2023-03-21 03:24:59 -0300

    refactor: chose method submit and inprove the code.

--- src/js/index.js
+++ src/js/index.js
@@ -4,57 +4,40 @@ let btn = document.querySelector('#btn')
 const form = document.getElementById('form')
 // const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
 
-inputs[0].addEventListener('change', () =>{
-        let name = inputs[0].value
-        let nameNumber = name.split(' ')
-        nameNumber = nameNumber.filter(item => item !== "")
+//car um jeito de chamar a função só ao apertar o submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+        
+ //Dar um jeito de pegar os valores do input e analizar se são incorretos ou corretos 
+    let name = inputs[0].value
+    let nameNumber = name.split(' ')
+    nameNumber = nameNumber.filter(item => item !== "")
 
-        if(nameNumber.length < 2){
-            validationFalse()    
-        }else{
-            validationTrue()
-        }
-})
-
-inputs[3].addEventListener('change', () =>{
     let content = inputs[3].value
     let contentNumber = content.split(' ')
     contentNumber = contentNumber.filter(item => item !== "")
 
-    if(contentNumber.length < 2){
-        validationFalse()    
-    }else{
-        validationTrue()
-    }
-})
-
-inputs[1].addEventListener('change', () =>{ 
     const email = inputs[1].value
     console.log(email)
     const emailRegex = new RegExp(
         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
     )
-    if(emailRegex.test(email)){
-        validationTrue()
-    }else{
-        validationFalse()
-    }
 
-
-    })
-
-    inputs[2].addEventListener('change', () =>{
-        const tel = inputs[2].value
-        console.log(tel)
-        const telRegex = new RegExp(
+    const tel = inputs[2].value
+    const telRegex = new RegExp(
             "^[0-9]"
-        )
-        if(telRegex.test(tel)){
-            validationTrue()
-        }else{
-            validationFalse()
-        }
-    })
+    )
+
+    if(nameNumber.length && contentNumber.length < 2){
+        
+        if(emailRegex.test(email) && telRegex.test(tel)){
+            validationFalse() 
+        }   
+    }else{
+        validationTrue()
+    }
+})
+
 // Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
 function validationFalse(){
 label.forEach(i =>{
@@ -82,16 +65,6 @@ function colorTrue(){
     inputs.classList.add("validation-true")
     })
     }
-
-
-form.addEventListener('submit', (event) => {
-    // event.preventDefault()
-})
-//Dar um jeito de pegar os valores do input e analizar se são incorretos ou corretos 
-
-//car um jeito de chamar a função só ao apertar o submit
-
-
 
 
 
