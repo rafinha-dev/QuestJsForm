0916c5389f56669311f27075f78f990d64a185f0
RafaelContact <contactrodrigues21@gmail.com>
2023-03-21 01:51:15 -0300

    fix: orgnizing functions

--- src/js/index.js
+++ src/js/index.js
@@ -2,44 +2,49 @@ let inputs = document.querySelectorAll('.input-color')
 let label = document.querySelectorAll('.label')
 let btn = document.querySelector('#btn')
 const form = document.getElementById('form')
+// const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
 
 inputs[0].addEventListener('change', () =>{
         let name = inputs[0].value
         let nameNumber = name.split(' ')
         nameNumber = nameNumber.filter(item => item !== "")
 
-        console.log(nameNumber)
         if(nameNumber.length < 2){
-            // Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
+            validationTrue()
             
-            label.forEach((i) =>{
-            i.innerHTML = 'campo obrigatório'
-            colorFalse()
-        })
-
-            function colorFalse(){
-                inputs.forEach((inputs) =>{
-                inputs.classList.remove("validation-true")
-            inputs.classList.add("validation-false")
-            })
-            }
         }else{
-           
-            // Quando um campo estiver preenchido a borda do input deve ficar verde
-            label.forEach((i) =>{
-            i.innerHTML = ""
-            colorTrue()
-            })
-
-            function colorTrue(){
-            inputs.forEach((inputs) =>{
-            inputs.classList.remove("validation-false")
-            inputs.classList.add("validation-true")
-            })
-            }
+            validationFalse()
         }
-
 })
+
+
+// Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
+function validationTrue(){
+label.forEach(i =>{
+    i.innerHTML = 'campo obrigatório'
+    colorFalse()
+})
+}
+function colorFalse(){
+    inputs.forEach((inputs) =>{
+    inputs.classList.remove("validation-true")
+inputs.classList.add("validation-false")
+})
+}
+
+function validationFalse(){
+    // Quando um campo estiver preenchido a borda do input deve ficar verde
+    label.forEach(i =>{
+    i.innerHTML = ""
+    colorTrue()
+    })
+}
+function colorTrue(){
+    inputs.forEach(inputs =>{
+    inputs.classList.remove("validation-false")
+    inputs.classList.add("validation-true")
+    })
+    }
 
 form.addEventListener('submit', (event) => {
     // event.preventDefault()
