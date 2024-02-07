07de3dfe8772c5a251d822bdd00e17f6d91e43be
RafaelContact <contactrodrigues21@gmail.com>
2024-02-06 22:19:25 -0300

    fix: add pattern to inputs

--- src/js/index.js
+++ src/js/index.js
@@ -2,70 +2,80 @@ let inputs = document.querySelectorAll('.input-color')
 let label = document.querySelectorAll('.label')
 let btn = document.querySelector('#btn')
 const form = document.getElementById('form')
+
+
+btn.addEventListener('submit', (e)=>{
+    e.preventDefault()
+})
+
+
+
+
+
 // const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
 
 //car um jeito de chamar a função só ao apertar o submit
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault()
         
  //Dar um jeito de pegar os valores do input e analizar se são incorretos ou corretos 
-    let name = inputs[0].value
-    let nameNumber = name.split(' ')
-    nameNumber = nameNumber.filter(item => item !== "")
+//     let name = inputs[0].value
+//     let nameNumber = name.split(' ')
+//     nameNumber = nameNumber.filter(item => item !== "")
 
-    let content = inputs[3].value
-    let contentNumber = content.split(' ')
-    contentNumber = contentNumber.filter(item => item !== "")
+//     let content = inputs[3].value
+//     let contentNumber = content.split(' ')
+//     contentNumber = contentNumber.filter(item => item !== "")
 
-    const email = inputs[1].value
-    console.log(email)
-    const emailRegex = new RegExp(
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-    )
+//     const email = inputs[1].value
+//     console.log(email)
+//     const emailRegex = new RegExp(
+//         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+//     )
 
-    const tel = inputs[2].value
-    const telRegex = new RegExp(
-            "^[0-9]"
-    )
+//     const tel = inputs[2].value
+//     const telRegex = new RegExp(
+//             "^[0-9]"
+//     )
 
 
-    if(nameNumber.length  < 2 || contentNumber.length < 2 ){  
-        validationFalse()
+//     if(nameNumber.length  < 2 || contentNumber.length < 2 ){  
+//         validationFalse()
              
-        }else if(emailRegex.test(email) && telRegex.test(tel)){
-            validationTrue()
-        }else{
-            validationFalse()
-        }
-})
+//         }else if(emailRegex.test(email) && telRegex.test(tel)){
+//             validationTrue()
+//         }else{
+//             validationFalse()
+//         }
+// })
 
 // Ao enviar o formulário, se o campo obrigatório não for preenchido a borda do input deve ficar vermelha e uma mensagem "campo obrigatório" também em vermelho deve aparecer embaixo do campo.
-function validationFalse(){
-label.forEach(i =>{
-    i.innerHTML = 'campo obrigatório'
-    colorFalse()
-})
-}
-function colorFalse(){
-    inputs.forEach(inputs =>{
-    inputs.classList.remove("validation-true")
-    inputs.classList.add("validation-false")
-})
-}
+// function validationFalse(){
+// label.forEach(i =>{
+//     i.innerHTML = 'campo obrigatório'
+//     colorFalse()
+// })
+// }
+// function colorFalse(){
+//     inputs.forEach(inputs =>{
+//     inputs.classList.remove("validation-true")
+//     inputs.classList.add("validation-false")
+// })
+// }
 
-function validationTrue(){
-    // Quando um campo estiver preenchido a borda do input deve ficar verde
-    label.forEach(i =>{
-    i.innerHTML = ""
-    colorTrue()
-    })
-}
-function colorTrue(){
-    inputs.forEach(inputs =>{
-    inputs.classList.remove("validation-false")
-    inputs.classList.add("validation-true")
-    })
-    }
+// function validationTrue(){
+//     // Quando um campo estiver preenchido a borda do input deve ficar verde
+//     label.forEach(i =>{
+//     i.innerHTML = ""
+//     colorTrue()
+//     })
+// }
+// function colorTrue(){
+//     inputs.forEach(inputs =>{
+//     inputs.classList.remove("validation-false")
+//     inputs.classList.add("validation-true")
+//     })
+//     }
 
 
 
